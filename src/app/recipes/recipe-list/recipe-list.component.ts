32ea59af40313795d3f5694eb6db5a32a92d6f86
k.prosenikov@blubito.com <k.prosenikov@blubito.com>
2018-04-19 13:36:34 +0300

    Implement and add models and make changes to components.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('cake', 'A very tasty cake',
+    'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe_images/recipe-image-legacy-id--1273545_8.jpg?itok=qM3FgbWW'),
+    new Recipe('new pastry', 'A very pastry cake',
+    'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/recipe_images/recipe-image-legacy-id--1273545_8.jpg?itok=qM3FgbWW')
+    ];
 
   constructor() { }
 
